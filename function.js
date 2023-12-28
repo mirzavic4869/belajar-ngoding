cf33eba36ec85f0648a29bc967bb9e32a4bfb718
Mirzam Avicena <avicenamirzam@gmail.com>
2023-12-29 02:38:22 +0700

    learn function and quiz 7.5

---
+++ function.js
@@ -0,0 +1,27 @@
+// FUNCTION
+
+// fungsi dapat mengembalikan nilai dengan menggunakan return
+// jika tidak menggunakan retur maka tidak ada nilai atau undifined
+
+// function helloWorld() {
+// 	return "hello world";
+// }
+
+// console.log(helloWorld());
+
+// function hiDunia() {
+// 	console.log("hi dunia");
+// }
+
+// console.log(hiDunia());
+
+// function helloSayang() {
+// 	console.log("hello sayang");
+// 	return helloSayang;
+// }
+// helloSayang();
+
+function heiBro() {
+	return "hei bro";
+}
+heiBro(); //tidak keluar apa apa karena tidak ada console.log
